6e3e0be4eba93308fb50e0391d69cf19f2a4a6b9
Lisa Shi <qinhang.shi@kuka.com>
2021-02-16 02:49:07 +0800

    homework

---
+++ Week_03/236.js
@@ -0,0 +1,17 @@
+var lowestCommonAncestor = function(root, p, q) {
+  if (root === null) {
+      return null;
+  }
+  if (root === p || root === q) {
+      return root;
+  }
+
+  let left = lowestCommonAncestor(root.left, p, q);
+  let right = lowestCommonAncestor(root.right, p, q);
+
+  if (left && right) {
+      return root;
+  }
+  
+  return left === null ? right : left;
+};
